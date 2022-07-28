76c6e57b47670013473209b295ef503ae3f0399c
Fares Alassiri <74058891+Fares-Alassiri@users.noreply.github.com>
2022-07-28 12:12:36 +0300

    fixing lint problems

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,9 +2,11 @@
     Calculator Functions
 """
 
+
 def add(x, y):
     """Add x to y and return the result."""
     return x + y
+
 
 def sub(x, y):
     """Subtract x from y and return the result."""
--- app/app/tests.py
+++ app/app/tests.py
@@ -9,17 +9,18 @@ from app import calc
 
 class CalcTests(SimpleTestCase):
     """Test the calc module."""
-    
+
     def test_add_function(self):
         """Test adding numbers together."""
         x = 5
         y = 6
         res = calc.add(x, y)
         self.assertEqual(res, 11)
-        
+
     def test_sub_function(self):
         """Test subtracting number from number."""
         x = 10
         y = 15
         res = calc.sub(x, y)
         self.assertEqual(res, 5)
+        
